a5b0aa4e2e307340f51130e039eedbe2a5ab14ff
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-15 16:37:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli=[]
+def line(katz_deli)
+  place_in_line=[]
+  if katz_deli=0
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index.each do |name, index|
+      position="#{index}. #{name} "
+      place_in_line<<position
+    end
+    puts "The line is currently: #{place_in_line}"
+  end
+end
