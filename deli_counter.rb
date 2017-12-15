00c2fd5fd03a45d866dc9b615974cab9a0767907
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-15 16:55:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli=[]
+def line(katz_deli)
+  place_in_line="The line is currently:"
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    katz_deli.each.with_index do |name, index|
+      place_in_line<<" #{index+1}. #{name}"
+    end
+    puts place_in_line
+  end
+end
+def take_a_number(katz_deli,name)
+  katz_deli<<name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
