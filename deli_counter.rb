aec5ad0236fcb6f625c592f749c1b146ad483bfc
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-15 16:49:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ def line(katz_deli)
 end
 def take_a_number(katz_deli,name)
   katz_deli<<name
-  puts "Welcome #{name}. You are number #{index+1} in line."
+  puts "Welcome #{name}. You are number #{katz_deli.length} in line."
 end
